750f668a094d9307a2c352af6d62fb338f5781cd
Cristian <zambranom.cristian09@gmail.com>
2024-07-01 19:48:10 +0200

    empezamos JS

---
+++ swiper/assets/js/js.js
@@ -0,0 +1,13 @@
+let isAnimating = false;
+let cardPullDistance = 0; // distancia que la carta se está arrastrando
+
+
+function startDrag (event){
+    if(isAnimating) return
+
+    const actualCard = event.target.closest('article') //cogemos el primer articulo ( carta)
+
+    //
+
+
+}
